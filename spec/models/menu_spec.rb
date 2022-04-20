2253c08a6be2e7ede52c8bcde3fcd35292896288
adiatma85 <adiatma85@gmail.com>
2022-04-20 22:44:32 +0700

    add menu spec and green and modify factory bot that got error

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.describe Menu, type: :model do
   # Test case where it is valid of Name, Description, and Price
   it 'is valid with a name, description and price' do
-    expect(FactoryBot.build(:food)).to be_valid
+    # Some error when using factory bot
+    # expect(FactoryBot.build(:menu)).to be_valid
+    food = Menu.new(
+      name: 'Nasi Uduk',
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 15000.0
+    )
+
+    expect(food).to be_valid
   end
 
   # Test case where is is not valid when name attributes is not exist
   it `is invalid without 'name' attribute` do
-    food = Food.new(
+    food = Menu.new(
       name: nil,
       description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
       price: 15000.0
@@ -19,7 +27,7 @@ RSpec.describe Menu, type: :model do
 
   # Test case where is is not valid when description attributes is not exist
   it `is invalid without 'description' attribute` do
-    food = Food.new(
+    food = Menu.new(
       name: 'Nasi Uduk',
       description: nil,
       price: 15000.0
@@ -30,7 +38,7 @@ RSpec.describe Menu, type: :model do
 
   # Test case where is is not valid when price attributes is not exist
   it `is invalid without 'price' attribute` do
-    food = Food.new(
+    food = Menu.new(
       name: 'Nasi Uduk',
       description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
       price: nil
@@ -41,7 +49,7 @@ RSpec.describe Menu, type: :model do
 
   # Test case where is is not valid when price attributes is non-numerical
   it `is invalid when 'price' attribute is non-numerical` do
-    food = Food.new(
+    food = Menu.new(
       name: 'Nasi Uduk',
       description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
       price: "non-numerical value"
@@ -52,7 +60,7 @@ RSpec.describe Menu, type: :model do
 
   # Test case where is is not valid when price attributes is >= 0.01
   it `is valid when 'price' attribute >= 0.01` do
-    food = Food.new(
+    food = Menu.new(
       name: 'Nasi Uduk',
       description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
       price: 10000.0
@@ -62,7 +70,7 @@ RSpec.describe Menu, type: :model do
 
   # Test case where is is not valid when price attributes is < 0.01
   it `is invalid when 'price' attribute < 0.01` do
-    food = Food.new(
+    food = Menu.new(
       name: 'Nasi Uduk',
       description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
       price: 0.005
@@ -73,13 +81,13 @@ RSpec.describe Menu, type: :model do
 
   # Test case where to test the name attrribute for each row is guaranted for unique
   it 'is invalid with a duplicate name' do
-    food1 = Food.create(
+    food1 = Menu.create(
       name: 'Nasi Uduk',
       description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
       price: 15000.0
     )
 
-    food2 = Food.new(
+    food2 = Menu.new(
       name: 'Nasi Uduk',
       description: 'Just with different description',
       price: 15000.0
